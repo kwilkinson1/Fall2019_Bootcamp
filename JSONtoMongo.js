8a7a5e123ce36d4d41daefc2e9102dabdcfb02f2
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2019-09-10 21:25:38 -0400

    Successfully connected to mongoDB and added all queries

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,24 +7,18 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config'),
-    listingData;
+    config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
-const connectDB = () => {
-  try {
-    await mongoose.connect(config.db.uri, {useNewUrlParser: true});
-    console.log('MongoDB connected...')
-  } catch(err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+mongoose.connect(config.db.uri, {useNewUrlParser: true,  useUnifiedTopology: true });
 
-}
+var db = mongoose.connection;
 
-connectDB();
+db.once("open", () =>{
+  console.log("Connection successful!");
+});
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
@@ -33,16 +27,55 @@ connectDB();
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
 
-  fs.readFile('listings.json', 'utf8', function(err, data) {
+  // This parses the JSON file into JSON data for insertion into the database
+  //var listingData = JSON.parse(fs.readFileSync('listings.json', 'utf8'));
 
-    //Check for errors
-    if(err) throw err;
-
-    listingData = JSON.parse(data);
-
-  })
+  var listing, buildings;
+  //First, create a function to read the file and convert it into JSON data
+  //I made this function synchronous, so that the data would be available 
+  //before attempting to connect to mongoDB
+  var data = fs.readFileSync('listings.json', 'utf-8');
+    try {
+      let listingData;
+      
+      // Use JSON.parse to convert file into JSON object
+      listingData = JSON.parse(data);
+          
+      // Iterate through the listingData to use the schema to model the 
+      // data prior to adding to mongoDB
+      for (let i = 0; i < listingData.entries.length; i++) {
+        let building = new Listing(listingData.entries[i]);
+        
+        // Add each building to mongoDB using mongoose
+        Listing.create(building, function(err, doc){
+          if (err) {
+            return console.error(err.message);
+          }
+          console.log(doc);
+          return db.close;
+        })
+      }
+      //console.log(listingData.entries[0]);
+      
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  
+  
+  /*async function insertListings() {
+    try {
+      await db.insertMany(listingData);
+      console.log('Finished updating!');
+      process.exit();
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  }*/
 
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+
--- queries.js
+++ queries.js
@@ -1,6 +1,17 @@
 /* Add all the required libraries*/
-
+var fs = require('fs'),
+    mongoose = require('mongoose'),  
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
 /* Connect to your database using mongoose - remember to keep your key secret*/
+
+mongoose.connect(config.db.uri, {useNewUrlParser: true,  useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+db.once("open", () =>{
+  console.log("Connection successful!");
+});
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,6 +21,13 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({'name': 'Library West'}, (err, building) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(building);
+  })
+
 };
 var removeCable = function() {
   /*
@@ -17,20 +35,39 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.find({'code': 'CABL'}, (err, courses) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(courses);
+  })
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Lab address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.updateOne({"name": "Phelps Laboratory"}, {"address": "1953 Museum Rd, Gainesville, FL 32603"}, (err, updatedDoc) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(updatedDoc);
+  })
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+   Listing.find({}, (err, listings) => {
+     if (err) {
+       console.error(err.message);
+     }
+     console.log(listings);
+   })
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
